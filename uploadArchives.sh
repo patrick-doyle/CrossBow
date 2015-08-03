1e54f8a7f9c59385c614a533118cf95854786eb1
Patrick <patrick@twistedequastions.com>
2015-08-03 01:38:16 +0100

    - Updated some Metadata files and the readme.

--- uploadArchives.sh
+++ uploadArchives.sh
@@ -1,3 +1,4 @@
-gradlew :Libraries:CrossBow:build :Libraries:CrossBow:bintrayUpload
-gradlew :Libraries:CrossBow-Wear:build :Libraries:CrossBow-Wear:bintrayUpload
-gradlew :Libraries:Crossbow-Wear-Receiver:build :Libraries:Crossbow-Wear-Receiver:bintrayUpload
+./gradlew copyReadme
+./gradlew :Libraries:CrossBow:build :Libraries:CrossBow:bintrayUpload
+./gradlew :Libraries:CrossBow-Wear:build :Libraries:CrossBow-Wear:bintrayUpload
+./gradlew :Libraries:Crossbow-Wear-Receiver:build :Libraries:Crossbow-Wear-Receiver:bintrayUpload
